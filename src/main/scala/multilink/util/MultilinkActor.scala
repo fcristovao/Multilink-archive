ed71ec5fb8f79ce98801f99a3c27b3fed31a69df
Filipe Cristóvão <filipe.md.cristovao@gmail.com>
2012-06-30 12:28:05 +0100

    Adds LoggingReceive to MultilinkActor.
    However, the loggingReceive is only in the react. That means that any stackable modifications won't have the logging activated to them. I'll see if there's a workaround this.

--- src/main/scala/multilink/util/MultilinkActor.scala
+++ src/main/scala/multilink/util/MultilinkActor.scala
@@ -1,11 +1,12 @@
 package multilink.util
 
 import akka.actor.Actor
+import akka.event.LoggingReceive
 
-trait MultilinkActor extends Actor {
+trait MultilinkActor extends Actor{
 	
 	def react : Receive
 	
-	def receive = react
+	def receive = LoggingReceive(react)
 }
 
