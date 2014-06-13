0b1b614ae9f6d523bcc97753fc77d6fbd4bec3c6
Filipe Cristóvão <filipe.md.cristovao@gmail.com>
2014-06-13 17:57:29 +0200

    Refactors ComposableFSM

--- src/main/scala/multilink/util/composition/ComposableFSM.scala
+++ src/main/scala/multilink/util/composition/ComposableFSM.scala
@@ -7,22 +7,18 @@ import multilink.util.composition.channels.Channel.{Done, Process}
 trait ComposableFSM[S, D] extends FSM[S, D] with Composable {
   this: Actor =>
 
-  protected final val ender: StateFunction = {
+  protected val ender: StateFunction = {
     case _ => stay()
   }
 
   def whenIn(stateName: S, stateTimeout: FiniteDuration = null)(stateFunction: StateFunction) = {
     val interceptor: StateFunction = {
       case Event(Process(generation, thisNode, msg), stateData) => {
-        val newState = (stateFunction orElse ender)(Event(msg, stateData))
-        val doneMsg = List(Done(generation, thisNode, msg))
-        newState.replies match {
-          case Nil => newState.copy(replies = doneMsg)
-          case anythingElse => newState.copy(replies = newState.replies ::: doneMsg)
-        }
+        val doneMsg = Done(generation, thisNode, msg)
+        (stateFunction orElse ender)(Event(msg, stateData)) replying doneMsg
       }
     }
-    super.when(stateName, stateTimeout)(interceptor orElse stateFunction)
+    super.when(stateName, stateTimeout)(interceptor)
   }
 
 }
