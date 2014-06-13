e1f04eeea0b5f131c8fdfe1b27c2fcfa9d08940b
Filipe Cristóvão <filipe.md.cristovao@gmail.com>
2014-06-13 18:07:26 +0200

    Adds the InternetPointAddress to the Gateway, so that it only responds to it's own

--- src/main/scala/multilink/game/network/intranet/Gateway.scala
+++ src/main/scala/multilink/game/network/intranet/Gateway.scala
@@ -1,9 +1,10 @@
 package multilink.game.network.intranet
 
-import akka.actor.{Actor, LoggingFSM}
+import akka.actor.{Props, Actor, LoggingFSM}
 import scala.concurrent.duration._
 
 import multilink.util.composition.ComposableFSM
+import multilink.game.network.internet.InternetPointAddress
 
 object Gateway {
   sealed trait State
@@ -17,11 +18,16 @@ object Gateway {
 	case class Connected(from: Int, to: Int) extends Messages
 	case object DisableGateway extends Messages
 	case object EnableGateway extends Messages
+  case object ConnectionRefused extends Messages
+
+  def apply(ip: InternetPointAddress) = {
+    Props(classOf[Gateway], ip)
+  }
 }
 
 
 
-class Gateway() extends Actor with ComposableFSM[Gateway.State, Unit] with LoggingFSM[Gateway.State, Unit] {
+class Gateway(ip: InternetPointAddress) extends Actor with ComposableFSM[Gateway.State, Unit] with LoggingFSM[Gateway.State, Unit] {
   import Gateway._
 
   startWith(Enabled, Unit)
@@ -29,12 +35,14 @@ class Gateway() extends Actor with ComposableFSM[Gateway.State, Unit] with Loggi
   whenIn(Enabled) {
     case Event(DisableGateway,_) =>
       goto(Disabled) forMax (2 seconds) replying(DisableGateway) replying ("hello")
-    case Event(Route(from, through, to),_) =>
+    case Event(Route(from, through, to),_) if ip == through =>
     	stay replying Routed(from, through, to)
-    case Event(Connect(from, to),_) =>
+    case Event(Connect(from, to),_) if ip == to =>
     	stay replying Connected(from, to)
     case Event(StateTimeout,_) =>
       goto(Disabled) forMax (2 seconds)
+    case _ =>
+      stay replying ConnectionRefused
   }
 
   whenIn(Disabled) {
