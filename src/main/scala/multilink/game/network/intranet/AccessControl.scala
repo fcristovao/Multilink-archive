ff21bbb6b548ca56b042e3b93eba5fa15d713ce4
Filipe Cristóvão <filipe.md.cristovao@gmail.com>
2014-06-14 20:39:03 +0200

    Renames the LoginSystem to AccessControl and adds the proper spec for it

---
+++ src/main/scala/multilink/game/network/intranet/AccessControl.scala
@@ -0,0 +1,74 @@
+package multilink.game.network.intranet
+
+import multilink.util.composition.ComposableFSM
+import multilink.game.network.intranet.AccessControl.{Config, Password, Username}
+import scala.collection.immutable.Map
+import akka.actor.{ActorRef, Props}
+
+object AccessControl {
+  import scala.concurrent.duration._
+
+  type Username = String
+  type Password = String
+
+
+  sealed trait Messages
+  case class AddUsernameAndPassword(username: Username, password: Password) extends Messages
+  case class Login(username: Username, password: Password) extends Messages
+  case class AccessGranted(username: Username) extends Messages
+  case object AccessDenied extends Messages
+  case class SessionTimeout(actor: ActorRef) extends Messages
+
+  sealed trait State
+  case object Active extends State
+
+  case class Data(credentials: Map[Username, Password], sessions: Set[ActorRef])
+
+  def apply(credentials: Map[Username, Password])(implicit config: Config = Config()) = {
+    Props(classOf[AccessControl], credentials, config)
+  }
+
+  case class Config(sessionTimeout: FiniteDuration = 60 seconds)
+
+}
+
+class AccessControl(initialDb: Map[Username, Password], config: Config) extends ComposableFSM[AccessControl.State, AccessControl.Data] {
+  import AccessControl._
+
+  startWith(Active, Data(initialDb, Set()))
+
+  whenIn(Active) {
+    case Event(Login(username, tryPassword), data@Data(credentials, sessions)) =>
+      credentials.get(username) match {
+        case None => // Unknown user
+          sender() ! AccessDenied
+          stay()
+        case Some(password) if password != tryPassword => // wrong password
+          sender() ! AccessDenied
+          stay()
+        case Some(password) if password == tryPassword => // correct password
+          setTimer(sender().path.toString, SessionTimeout(sender()), config.sessionTimeout)
+          sender() ! AccessGranted(username)
+          stay() using data.copy(sessions = sessions + sender())
+
+      }
+    case Event(AddUsernameAndPassword(username, password), data@Data(credentials, _)) => {
+      stay() using data.copy(credentials = credentials + (username -> password))
+    }
+    case Event(SessionTimeout(timedOutSession), data@Data(_, sessions)) => {
+      stay() using data.copy(sessions = sessions - timedOutSession)
+    }
+    case Event(msg, Data(_, sessions)) => {
+      if (sessions.contains(sender())) {
+        //renew the session timer:
+        setTimer(sender().path.toString, SessionTimeout(sender()), config.sessionTimeout)
+        stay()
+      } else {
+        stay() replying AccessDenied
+      }
+    }
+  }
+
+  override def toString = "AccessControl"
+}
+
---
+++ src/test/scala/multilink/network/intranet/AccessControlSpec.scala
@@ -0,0 +1,76 @@
+package multilink.network.intranet
+
+import akka.testkit.{ImplicitSender, TestKit}
+import akka.actor.ActorSystem
+import com.typesafe.config.ConfigFactory
+import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
+import scala.collection.immutable
+import multilink.game.network.intranet.AccessControl.{AccessDenied, Login, AccessGranted}
+import multilink.game.network.intranet.AccessControl
+import multilink.util.composition._
+import ArrowOperator._
+
+class AccessControlSpec extends TestKit(ActorSystem("test", ConfigFactory.load("application-test")))
+                                with WordSpecLike with ImplicitSender with BeforeAndAfterAll {
+
+  case class Hello() extends ComposableActor {
+    def react = {
+      case "Hello" => sender() ! "HelloBack"
+    }
+  }
+
+  val defaultCredentials = immutable.Map(
+    "admin" -> "12345",
+    "anotherUser" -> "54321"
+  )
+
+  override def afterAll() {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "An Access Control" should {
+    "deny access for an unknown user" in {
+      val channel = openChannelFor(AccessControl(defaultCredentials) >>> Hello())
+      channel ! Login("qwerty", "12345")
+      expectMsg(AccessDenied)
+    }
+    "deny access for an known user but incorrect password" in {
+      val channel = openChannelFor(AccessControl(defaultCredentials) >>> Hello())
+      channel ! Login("admin", "qwerty")
+      expectMsg(AccessDenied)
+    }
+    "grant access if given the correct credentials" in {
+      val channel = openChannelFor(AccessControl(defaultCredentials) >>> Hello())
+      channel ! Login("admin", "12345")
+      expectMsg(AccessGranted("admin"))
+    }
+    "deny access further in the network when no login was done" in {
+      val channel = openChannelFor(AccessControl(defaultCredentials) >>> Hello())
+      channel ! "Hello"
+      expectMsg(AccessDenied)
+      expectNoMsg()
+    }
+    "allow access further in the network after proper login was done" in {
+      val channel = openChannelFor(AccessControl(defaultCredentials) >>> Hello())
+      channel ! Login("admin", "12345")
+      expectMsg(AccessGranted("admin"))
+      channel ! "Hello"
+      expectMsg("HelloBack")
+    }
+    "timeout logged in sessions after a configured duration" in {
+      import scala.concurrent.duration._
+      val config = AccessControl.Config(1 seconds)
+      val channel = openChannelFor(AccessControl(defaultCredentials)(config) >>> Hello())
+      channel ! Login("admin", "12345")
+      expectMsg(AccessGranted("admin"))
+      channel ! "Hello"
+      expectMsg("HelloBack")
+      within(config.sessionTimeout, config.sessionTimeout + (3 seconds)) {
+        Thread.sleep((config.sessionTimeout + (1 seconds)).toMillis)
+        channel ! "Hello"
+        expectMsg(AccessDenied)
+        expectNoMsg()
+      }
+    }
+  }
+}
